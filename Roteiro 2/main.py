b8774099fa58fffd3c6acc5af4682b9b38387c6d
Gabriel Lima <gabriel1997lima@gmail.com>
2019-08-29 09:46:48 -0300

    Update main.py

--- Roteiro 2/main.py
+++ Roteiro 2/main.py
@@ -5,7 +5,7 @@ grafoParaiba = Grafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'],
                 {'a1': 'J-C', 'a2': 'C-E', 'a3': 'C-E', 'a4': 'C-P', 'a5': 'C-P', 'a6': 'C-M', 'a7': 'C-T', 'a8': 'M-T',
                  'a9': 'T-Z'})
 
-grafoQuestao = Grafo(['A','B','C','D','E','F','G','H','I','J','K'],{'a1': 'A-B','   a2': 'A-G','a3': 'A-J','a4': 'K-G','a5': 'K-J','a6': 'J-G','a7': 'J-I','a8': 'I-G','a9': 'G-H','a10': 'H-F','a11': 'F-B','a12': 'B-G','a13': 'B-C','a14': 'C-D','a15': 'D-E','a16': 'D-B','a17': 'B-E',})
+grafoQuestao = Grafo(['A','B','C','D','E','F','G','H','I','J','K'],{'a1': 'A-B','   a2': 'A-G','a3': 'A-J','a4': 'G-K','a5': 'K-J','a6': 'J-G','a7': 'I-J','a8': 'I-G','a9': 'G-H','a10': 'H-F','a11': 'F-B','a12': 'B-G','a13': 'B-C','a14': 'C-D','a15': 'D-E','a16': 'D-B','a17': 'B-E',})
 
 visitados = []
 print(grafoParaiba.DFS("J", visitados))
