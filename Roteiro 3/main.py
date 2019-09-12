72b8962356cb6df80eb588bcb09b46c2082a040c
Gabriel Lima <gabriel1997lima@gmail.com>
2019-09-11 23:44:11 -0300

    Update main.py

--- Roteiro 3/main.py
+++ Roteiro 3/main.py
@@ -1,14 +1,48 @@
 import unittest
 from grafo import *
 
-grafoBonitinho = Grafo(['A', 'B', 'C', 'D', 'E', 'F', 'G','Z','H','X'],
-                {'a1': 'A-B', 'a2': 'B-G', 'a3': 'G-H', 'a4': 'B-C', 'a5': 'C-D', 'a6': 'D-E', 'a7': 'E-F', 'a8': 'F-A', 'a9':'X-Z'})
+
+
+# QUESTAO 1:
+
+trueCase = Grafo(['A', 'B', 'C', 'D', 'E', 'F'],
+                 {'a1': 'A-B', 'a2': 'B-C', 'a3': 'C-D', 'a4': 'D-E', 'a5': 'E-F','a6':'A-F'})
+
+testeSimplesTrue = Grafo(['A', 'B', 'C'],{'a1': 'A-B', 'a2': 'B-C','a3':'C-A'})
+testeSimplesFalse = Grafo(['A', 'B', 'C'],{'a1': 'A-B', 'a2': 'B-C'})
+print(testeSimplesTrue.ha_ciclo())
+print(testeSimplesFalse.ha_ciclo())
+
+
+
+# QUESTAO 2
+
+GRAFO1 = Grafo(['A', 'B', 'C'],{'a1': 'A-B', 'a2': 'B-C','a3':'C-A'})
+GRAFO2 = Grafo(['A', 'B', 'C', 'D'],{'a1': 'A-B', 'a2': 'B-D','a3':'D-C'})
+
+grafoParaiba = Grafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'], {'a1':'J-C', 'a2':'C-E', 'a3':'C-E', 'a4':'C-P', 'a5':'C-P', 'a6':'C-M', 'a7':'C-T', 'a8':'M-T', 'a9':'T-Z'})
 
 
 
 
-analizados = []
-print(grafoBonitinho.ha_caminho('E','D',analizados))
+print(GRAFO1.caminho(3))
+print(GRAFO1.caminho(2))
+print(GRAFO2.caminho(3))
+print(grafoParaiba.caminho(5))
+print(grafoParaiba.caminho(7))
+
+
+
+# QUESTAO 3
+trueCase = Grafo(['A', 'B', 'C', 'D', 'E', 'F'],
+                {'a1': 'A-B', 'a2': 'A-C', 'a3': 'C-D', 'a4': 'C-E', 'a5': 'E-F', 'a6': 'B-F',})
+falseCase = Grafo(['A', 'B', 'C', 'D', 'E', 'F'],
+
+                {'a1': 'A-B', 'a2': 'A-E', 'a3': 'C-D', 'a5': 'E-F', 'a6': 'B-F',})
+
+
+print(trueCase.conexo())
+print(falseCase.conexo())
 
 
 
