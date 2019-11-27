726717b9540f8da7e231b88475f0aa47c08b49cb
Gabriel Lima <gabriel1997lima@gmail.com>
2019-11-27 16:09:34 -0300

    Add files via upload

---
+++ Roteiro 8/main.py
@@ -0,0 +1,11 @@
+from grafo import Grafo
+
+
+
+
+grafoDoGoogle = Grafo(["A","B","C","D","E","F","G"],{'A-B': 1,'A-C':4,'B-F': 2,'C-G':2,'G-F':1,'F-E':2,'D-E':3,'D-G':4})
+outroGrafoDoGoogle = Grafo(["A","B","C","D","E","F","G"],{'A-B': 1,'A-C':4,'B-F': 2,'C-G':2,'F-G':1,'F-E':2,'D-E':3,'D-G':4})
+
+
+
+grafoDoGoogle.algoritimo_de_PRIM()
